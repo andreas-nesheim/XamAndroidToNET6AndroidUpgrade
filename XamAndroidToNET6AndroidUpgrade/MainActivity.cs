e0869d8416781d6edafbe70ef32bdf738da8e272
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-06-13 20:48:19 +0200

    removed redundant Android resources. Now builds!

--- XamAndroidToNET6AndroidUpgrade/MainActivity.cs
+++ XamAndroidToNET6AndroidUpgrade/MainActivity.cs
@@ -1,25 +1,15 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Runtime;
-using AndroidX.AppCompat.App;
-
+﻿
 namespace XamAndroidToNET6AndroidUpgrade
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
